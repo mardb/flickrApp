634ddd004971cfe3e7b557b26a126740c60b179f
Marlene Guzman <marleneguzmanla@gmail.com>
2019-05-13 20:59:59 -0700

    building the HTML to display photos on page using jQuery $.each() method

--- flickr.js
+++ flickr.js
@@ -9,7 +9,16 @@ $(document).ready(function(){
       format: "json" 
     };
     function displayPhotos(data){
-      
+      var photoHTML = '<ul>';
+      //loop
+      //array, index, item
+      $.each(data.items, function(i, photo){
+    photoHTML += '<li class="grid-25 table-grid-50">';
+    photoHTML += '<a href="' + photo.link + ' " class="image">';
+    photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      });
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML);
     }
     $.getJSON(flickerAPI, flickrOptions, displayPhotos);
   });
