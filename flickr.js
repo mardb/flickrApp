4956f6b9982619ad01351a6ed838eeac4b18feea
Marlene Guzman <marleneguzmanla@gmail.com>
2019-05-13 20:35:02 -0700

    making a JSONP request of the Flickr web server

--- flickr.js
+++ flickr.js
@@ -2,5 +2,16 @@ $(document).ready(function(){
   $('button').click(function(){
     $("button").removeClass("selected");
     $(this).addClass("selected");
+    var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var animal = $(this).text();
+    var flickrOptions = { 
+      tags: animal,
+      format: "json" 
+    };
+    function displayPhotos(data){
+      
+    }
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
   });
+
 }); //end ready
